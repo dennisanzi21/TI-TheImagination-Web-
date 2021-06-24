de93713f734d2ce5b520d2a8a3a7c2682db621ea
Darken_Cat <61375431+dennisanzi21@users.noreply.github.com>
2021-06-24 14:30:10 +0800

    update

---
+++ TI(The Imagination)/TI-TheImagination-Hackathon/TIpay.js
@@ -0,0 +1,36 @@
+var modal = document.getElementById('reload_modal');
+var btn = document.getElementById('reloadbtn');
+var modal2 = document.getElementById('reload_modal2');
+var btn2 = document.getElementById('reloadbtn2');
+var modal3 = document.getElementById('reload_modal3');
+var btn3 = document.getElementById('reloadbtn3');
+var modal4 = document.getElementById('elec_modal');
+var btn4 = document.getElementById('elecbtn');
+var modal5 = document.getElementById('elec_modal2');
+var btn5 = document.getElementById('elecbtn2');
+var modal6 = document.getElementById('elec_modal3');
+var btn6 = document.getElementById('elecbtn3');
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}  
+ 
+btn2.onclick = function() {
+    modal2.style.display = "block";
+}
+
+btn3.onclick = function() {
+    modal3.style.display = "block";
+} 
+
+btn4.onclick = function() {
+    modal4.style.display = "block";
+}
+
+btn5.onclick = function() {
+    modal5.style.display = "block";
+}
+
+btn6.onclick = function() {
+    modal6.style.display = "block";
+}
